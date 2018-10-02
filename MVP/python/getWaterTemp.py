45c6c38fb4035bbc0f2dfe391fd44aac87a8c8c5
Eduardo Moreno <eduardo@niubit.es>
2018-10-02 16:28:50 +0200

    Python script for reading temperature of water with DS18B20 probes ( http://carlini.es/sensor-de-temperatura-ds18b20-en-la-raspberry-pi/ ).

---
+++ MVP/python/getWaterTemp.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+YELLOW_DEVICE = "/sys/bus/w1/devices/28-02129177fa36/w1_slave"
+WHITE_DEVICE = "/sys/bus/w1/devices/28-020d9177c739/w1_slave"
+
+def get_temp_sens(device):
+        tfile = open(device)
+        text = tfile.read()
+        tfile.close()
+        secondline = text.split("\n")[1]
+        temperaturedata = secondline.split(" ")[9]
+        temperature = float(temperaturedata[2:])
+        temperature = temperature / 1000
+        return float(temperature)
+
+mensaje = str(get_temp_sens(YELLOW_DEVICE)) + " ºC"
+
+print(mensaje)
